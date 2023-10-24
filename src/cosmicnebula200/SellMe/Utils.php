ffeb671512cb58b6f9369d4c4eccaef2ee5852f9
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-23 20:56:51 -0700

    Update Utils.php

--- src/cosmicnebula200/SellMe/Utils.php
+++ src/cosmicnebula200/SellMe/Utils.php
@@ -3,7 +3,7 @@
 namespace cosmicnebula200\SellMe;
 
 use pocketmine\item\Item;
-use pocketmine\item\ItemFactory;
+use pocketmine\item\StringToItemParser;
 use pocketmine\player\Player;
 
 class Utils
@@ -13,57 +13,69 @@ class Utils
     {
         $amount = self::getAmount($item);
         if ($amount === 0)
-        return false;
-	SellMe::getInstance()->getEconomyProvider()->addToMoney($player, $amount * $item->getCount(), [
-		"item" => $item->getVanillaName(),
-		"amount" => $item->getCount(),
-	]);
+            return false;
+        
+        SellMe::getInstance()->getEconomyProvider()->addToMoney($player, $amount * $item->getCount(), [
+            "item" => $item->getVanillaName(),
+            "amount" => $item->getCount(),
+        ]);
+        
         return true;
     }
 
     public static function getAmount(Item $item): int
     {
-        if (SellMe::$prices->getNested("prices.{$item->getId()}:{$item->getMeta()}") != false)
-            return (int)SellMe::$prices->getNested("prices.{$item->getId()}:{$item->getMeta()}");
-        if (SellMe::$prices->getNested("prices.{$item->getId()}") !== false)
+        $parsedItem = StringToItemParser::getInstance()->parse("{$item->getId()}:{$item->getMeta()}");
+        
+        if ($parsedItem !== null && SellMe::$prices->getNested("prices.{$parsedItem->getId()}:{$parsedItem->getMeta()}") !== false) {
+            return (int)SellMe::$prices->getNested("prices.{$parsedItem->getId()}:{$parsedItem->getMeta()}");
+        }
+
+        if (SellMe::$prices->getNested("prices.{$item->getId()}") !== false) {
             return (int)SellMe::$prices->getNested("prices.{$item->getId()}");
+        }
+
         return 0;
     }
 
     public static function getName(string $data): string
     {
-        $id = explode(":", $data)[0] ?? $data;
-        $meta = explode(":", $data)[1] ?? 0;
-        $item = ItemFactory::getInstance()->get($id, $meta);
-        return $item->getName();
+        $parsedItem = StringToItemParser::getInstance()->parse($data);
+        
+        if ($parsedItem !== null) {
+            return $parsedItem->getName();
+        }
+
+        return "";
     }
     
     public static function addToPrices(Player $player, int $price, bool $overwrite = false): void
     {
         $item = $player->getInventory()->getItemInHand();
-        if (!$overwrite){
-            if (Utils::getAmount($item) !== 0) {
-                $player->sendMessage(SellMe::$messages->getMessage(
-                    'sell.error-adding',
-                    [],
-                    "The item already exits in prices, you can use '/sell overwrite' command to overwrite the price"
-                ));
-                return;
-            }
-        }
-        if ($price <= 0)
-        {
+        $string = "{$item->getId()}:{$item->getMeta()}";
+
+        if (!$overwrite && Utils::getAmount($item) !== 0) {
             $player->sendMessage(SellMe::$messages->getMessage(
-                'sell.non-positive',
+                'sell.error-adding',
                 [],
-                "The price can not be a non positive integer"
+                "The item already exists in prices. You can use '/sell overwrite' command to overwrite the price."
             ));
             return;
         }
-        $string = $item->getMeta() == 0 ? "{$item->getId()}" : "{$item->getId()}:{$item->getMeta()}";
+
+        if ($price <= 0) {
+            $player->sendMessage(SellMe::$messages->getMessage(
+                'sell.non-positive',
+                [],
+                "The price cannot be a non-positive integer."
+            ));
+            return;
+        }
+
         SellMe::$prices->setNested("prices.$string", $price);
         SellMe::$prices->save();
         SellMe::$prices->reload();
+        
         $player->sendMessage(SellMe::$messages->getMessage(
             'sell.added',
             [
@@ -73,5 +85,4 @@ class Utils
             'Added {ITEM} for {AMOUNT} to the list of prices'
         ));
     }
-    
 }
